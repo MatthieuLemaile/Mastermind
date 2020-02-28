2b78416aecf119f5e5275f613a60e379a5353986
Matthieu Lemaile <mathieu.lemaile@mymoneybank.com>
2020-02-28 09:46:30 +0100

    Added maven

---
+++ src/main/java/fr/lemaile/mastermind/AppMain.java
@@ -0,0 +1,11 @@
+package fr.lemaile.mastermind;
+
+import fr.lemaile.mastermind.controller.Game;
+
+public class AppMain {
+
+    public static void main(String[] args) {
+        new Game();
+    }
+
+}
